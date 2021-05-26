0afa5fdb27b54766877d94741f3b41c22e4b64f6
Danial Hasan <danialhasan14@gmail.com>
2021-05-26 19:21:10 -0400

    removed SPA plugin

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,9 @@ import {
 } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
-const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
 
 export default defineConfig({
-  plugins: [vue(),
-    new PrerenderSPAPlugin({
-      // Required - The path to the webpack-outputted app to prerender.
-      staticDir: path.join(__dirname, 'dist'),
-      // Required - Routes to render.
-      routes: ['/'],
-    })
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src'),
