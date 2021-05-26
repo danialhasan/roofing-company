454c1980cda402d218e5c25ac115e5d564374e7a
Danial Hasan <danialhasan14@gmail.com>
2021-05-26 19:03:14 -0400

    Added prerendering using prerender-spa-plugin to prerender HTML forms for Netlify Form build bots

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
-import { defineConfig } from 'vite'
+import {
+  defineConfig
+} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    new PrerenderSPAPlugin({
+      // Required - The path to the webpack-outputted app to prerender.
+      staticDir: path.join(__dirname, 'dist'),
+      // Required - Routes to render.
+      routes: ['/'],
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src'),
